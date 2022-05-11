d01c4cfa8b4808b7d6348097c4c9c3a825e85987
Artchibald <archie@archibaldbutler.com>
2022-05-11 15:51:22 +0100

    512 export correct

--- customScript.ts
+++ customScript.ts
@@ -1,10 +1,7 @@
 try {
-  function saveAsPNGAt512x512() {
+  function saveAsPNGAt512x512Icon1() {
     var pngFile = new File(
-      app.activeDocument.path +
-        "/" +
-        app.activeDocument.name.split(".")[0] +
-        ".png"
+      app.activeDocument.path + "/512x512/" + "icon1" + ".png"
     );
     var type = ExportType.PNG24;
     var opts = new ExportOptionsPNG24();
@@ -15,11 +12,43 @@ try {
     ExportOptionsPNG24.verticalScale = 512;
     app.activeDocument.exportFile(pngFile, type, opts);
   }
-  saveAsPNGAt512x512();
-  // hide icons layer test
-  // var doc = app.activeDocument;
-  // var ilayer = doc.layers.getByName("icons");
-  // ilayer.visible = false;
+  function saveAsPNGAt512x512Icon2() {
+    var pngFile = new File(
+      app.activeDocument.path + "/512x512/" + "icon2" + ".png"
+    );
+    var type = ExportType.PNG24;
+    var opts = new ExportOptionsPNG24();
+    ExportOptionsPNG24.antiAliasing = false;
+    ExportOptionsPNG24.transparency = true;
+    ExportOptionsPNG24.artBoardClipping = true;
+    ExportOptionsPNG24.horizontalScale = 512;
+    ExportOptionsPNG24.verticalScale = 512;
+    app.activeDocument.exportFile(pngFile, type, opts);
+  }
+  function saveAsPNGAt512x512Icon3() {
+    var pngFile = new File(
+      app.activeDocument.path + "/512x512/" + "icon3" + ".png"
+    );
+    var type = ExportType.PNG24;
+    var opts = new ExportOptionsPNG24();
+    ExportOptionsPNG24.antiAliasing = false;
+    ExportOptionsPNG24.transparency = true;
+    ExportOptionsPNG24.artBoardClipping = true;
+    ExportOptionsPNG24.horizontalScale = 512;
+    ExportOptionsPNG24.verticalScale = 512;
+    app.activeDocument.exportFile(pngFile, type, opts);
+  }
+  var bgLayer1 = app.activeDocument.layers[1];
+  var bgLayer2 = app.activeDocument.layers[2];
+  var bgLayer3 = app.activeDocument.layers[3];
+  bgLayer1.visible = true;
+  saveAsPNGAt512x512Icon1();
+  bgLayer1.visible = false;
+  bgLayer2.visible = true;
+  saveAsPNGAt512x512Icon2();
+  bgLayer2.visible = false;
+  bgLayer3.visible = true;
+  saveAsPNGAt512x512Icon3();
 } catch (err) {
   alert(err.message);
 }
